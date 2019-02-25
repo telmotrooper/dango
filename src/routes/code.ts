46f3da1fa2044fa7649309d3f112f73449d98626
Telmo Henrique Valverde <telmo.trooper@gmail.com>
2019-02-24 21:36:47 -0300

    Added PEG.js and an example grammar

--- src/routes/code.ts
+++ src/routes/code.ts
@@ -1,11 +1,24 @@
 import { Request, Response, Router } from "express"
+import fs from "fs"
+
+// TODO: Find a way to use "import from" syntax
+// tslint:disable-next-line:no-var-requires
+const peg = require("pegjs")
 
 const router: Router = Router()
+
+const grammar = fs.readFileSync(__dirname + "/../misc/grammar.pegjs", "utf-8")
+const parser = peg.generate(grammar)
 
 router.post("/", (req: Request, res: Response) => {
   const { codebox } = req.body
 
-  console.log(codebox)
+  // console.log("Code:\n" + codebox + "\n")
+  // console.log("Grammar:\n" + grammar)
+
+  console.log(
+    parser.parse(codebox),
+  )
 
   res.sendStatus(200)
 })
