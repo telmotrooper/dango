45565ecef611e0456c3ecb26d9497a1e6a3c83af
Telmo Henrique Valverde <telmo.trooper@gmail.com>
2019-03-18 23:11:45 -0300

    Changed slogan

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { Request, Response, Router } from "express"
 const router: Router = Router()
 
 router.get("/", (req: Request, res: Response) => {
-  res.render("index", { title: "Dango", message: "Write your code" })
+  res.render("index", { title: "Dango", message: "ER diagrams to graph databases" })
 })
 
 export const Index = router
