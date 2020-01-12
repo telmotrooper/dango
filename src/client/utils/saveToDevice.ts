6528c8fc3576790085c6f7f2d225046b46131ed9
Telmo "Trooper <telmo.trooper@gmail.com>
2020-01-11 21:35:12 -0300

    Removed semicolons

---
+++ src/client/utils/saveToDevice.ts
@@ -0,0 +1,14 @@
+const saveToDevice = (codebox: any) => {
+  const file = new Blob([codebox.current.value], {type: "text/plain"})
+  const url = URL.createObjectURL(file)
+
+  let a = document.createElement("a")
+  a.style.display = "none"
+  document.body.appendChild(a)
+
+  a.href = url
+  a.download = "er.txt"
+  a.click()
+}
+
+export { saveToDevice }
