65513a692b2a8765c5efa1421f5b37e58dcb2410
Telmo "Trooper <telmo.trooper@gmail.com>
2020-01-15 22:23:03 -0300

    Cleaning up

--- src/client/utils/requests.ts
+++ src/client/utils/requests.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosResponse } from "axios"
-import { RefObject } from "react"
 
 const submitCode = (code: string): Promise<AxiosResponse> =>
   axios.post("/er-code", {
@@ -10,16 +9,5 @@ const getCypherFromER = (code: string): Promise<AxiosResponse> =>
   axios.post("/get-cypher-from-er", {
     er: code,
   })
-
-// const getCypherFromER = async (textArea: RefObject<any>,
-//   setShow: (boolean) => void, setContent: (string) => void) => {
-  
-//   const res = await axios.post("/get-cypher-from-er", {
-//     er: textArea.current.value,
-//   })
-
-//   setContent(res.data)
-//   setShow(true)
-// }
 
 export { getCypherFromER, submitCode }
