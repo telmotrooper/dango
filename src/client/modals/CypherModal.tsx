901de32080a46281daa3e2d504da98779ad0d664
Telmo "Trooper <telmo.trooper@gmail.com>
2020-02-25 09:53:42 -0300

    Organizing code on CypherModal

--- src/client/modals/CypherModal.tsx
+++ src/client/modals/CypherModal.tsx
@@ -14,14 +14,18 @@ const CypherModal = React.memo((props: Props) => {
 
   return (
     <div className={"modal" + (show ? " is-active": "")} id="cypher-modal">
-      <div className="modal-background"></div>
       <div className="modal-card">
         <header className="modal-card-head">
-          <p className="modal-card-title"><b>Converter output</b> <i>(Cypher)</i></p>
-          <button className="delete" aria-label="close" onClick={(): void => setShow(false)}></button>
+          <p className="modal-card-title">
+            <b>Converter output</b> <i>(Cypher)</i>
+          </p>
+          <button className="delete" aria-label="close" onClick={(): void => setShow(false)} />
         </header>
         <section className="modal-card-body">
-          <p className="mb-05 ta-j">This is a schema for the Neo4j graph database (written in the Cypher query language) based on your Entity-Relationship Diagram:</p>
+          <p className="mb-05 ta-j">
+            This is a schema for the Neo4j graph database (written in the
+            Cypher query language) based on your Entity-Relationship Diagram:
+          </p>
           <textarea ref={textAreaRef} readOnly={true} value={content}
             className="textarea has-fixed-size is-small mb-1" rows={18} id="get-cypher-from-er" />
         </section>
@@ -30,8 +34,12 @@ const CypherModal = React.memo((props: Props) => {
             onClick={(): void => saveToDevice(textAreaRef, "cypher.txt")}>Save to device</button>
           
           <div>
-            <button className="button is-info">Generate visualization</button>
-            <button className="button is-success">Run in Neo4j instance</button>
+            <button className="button is-info">
+              Generate visualization
+            </button>
+            <button className="button is-success">
+              Run in Neo4j instance
+            </button>
           </div>
 
         </footer>
