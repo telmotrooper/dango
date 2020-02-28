d15fd0c33c5427c08b4f052d65045f069fa785a2
Telmo "Trooper <telmo.trooper@gmail.com>
2020-02-27 21:56:58 -0300

    Changed colors

--- src/client/utils/graphviz.ts
+++ src/client/utils/graphviz.ts
@@ -2,10 +2,11 @@ import { lower } from "../../shared/removeAccents"
 import { ER } from "../../server/misc/interfaces"
 
 const getEntity = (entityName: string): string =>
-  `${lower(entityName)} [label="${entityName}", shape=rectangle, style=filled]`
+  `${lower(entityName)} [label="${entityName}", shape=rectangle, style=filled, colorscheme=paired10, fillcolor=9]`
 
 const getAttribute = (entityName: string, attributeName: string, primaryKey?: boolean): string =>
-  `${lower(entityName + "_" + attributeName)} [label="${attributeName}", shape=${primaryKey ? "doublecircle" : "circle"}, fixedsize=true, height=0.5, width=0.5, fontsize=10]`
+  `${lower(entityName + "_" + attributeName)} [label="${attributeName}", shape=${primaryKey ? "doublecircle" : "circle"}, ` +
+  `style=filled, fixedsize=true, height=0.5, width=0.5, fontsize=10, fillcolor="#FFFEE0"]`
 
 const getConnection = (entityName: string, attributeName: string): string =>
   `${lower(entityName)} -- ${lower(entityName + "_" + attributeName)}`
