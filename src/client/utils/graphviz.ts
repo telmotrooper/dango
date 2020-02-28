1933fa19e641a9393af79e0f9cd9491c335119f3
Telmo "Trooper <telmo.trooper@gmail.com>
2020-02-27 21:34:14 -0300

    Working on diagram

--- src/client/utils/graphviz.ts
+++ src/client/utils/graphviz.ts
@@ -4,7 +4,11 @@ import { ER } from "../../server/misc/interfaces"
 const getEntity = (entityName: string): string =>
   `${lower(entityName)} [label="${entityName}", shape=rectangle, style=filled]`
 
-const getAttribute = (attributeName: string, primaryKey?: boolean): string => `${lower(attributeName)} [label="${attributeName}", shape=${primaryKey ? "doublecircle" : "circle"}, fixedsize=true, height=0.5, width=0.5, fontsize=10]`
+const getAttribute = (attributeName: string, primaryKey?: boolean): string =>
+  `${lower(attributeName)} [label="${attributeName}", shape=${primaryKey ? "doublecircle" : "circle"}, fixedsize=true, height=0.5, width=0.5, fontsize=10]`
+
+const getConnection = (entityName: string, attributeName: string): string =>
+  `${lower(entityName)} -- ${lower(attributeName)}`
 
 const convertER = (code: ER): string => {
   if (Object.entries(code).length === 0) {
@@ -15,9 +19,16 @@ const convertER = (code: ER): string => {
 
   for (const ent of code.ent) {
     diagram += getEntity(ent.id) + "\n"
+
+    for (const attribute of ent.data) {
+      diagram += getAttribute(attribute) + "\n"
+      diagram += getConnection(ent.id, attribute) + "\n"
+    }
   }
 
   diagram += "\n}"
+
+  console.debug(diagram)
 
   return diagram
 }
