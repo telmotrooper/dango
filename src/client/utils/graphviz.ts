03c78344406ac4d0a2bfd0b42825b1115db21168
Telmo "Trooper <telmo.trooper@gmail.com>
2020-02-27 22:33:00 -0300

    Added relationships

--- src/client/utils/graphviz.ts
+++ src/client/utils/graphviz.ts
@@ -1,15 +1,22 @@
 import { lower } from "../../shared/removeAccents"
 import { ER } from "../../server/misc/interfaces"
 
+const entityColor       = "#f8ec88"
+const attributeColor    = "#79bddc"
+const relationshipColor = "#dc9079"
+
 const getEntity = (entityName: string): string =>
-  `${lower(entityName)} [label="${entityName}", shape=rectangle, style=filled, colorscheme=paired10, fillcolor=9]`
+  `${lower(entityName)} [label="${entityName}", shape=rectangle, style=filled, fillcolor="${entityColor}"]`
 
 const getAttribute = (entityName: string, attributeName: string, primaryKey?: boolean): string =>
   `${lower(entityName + "_" + attributeName)} [label="${attributeName}", shape=${primaryKey ? "doublecircle" : "circle"}, ` +
-  `style=filled, fixedsize=true, height=0.5, width=0.5, fontsize=10, fillcolor="#FFFEE0"]`
+  `style=filled, fixedsize=true, height=0.5, width=0.5, fontsize=10, fillcolor="${attributeColor}"]`
 
 const getConnection = (entityName: string, attributeName: string): string =>
   `${lower(entityName)} -- ${lower(entityName + "_" + attributeName)}`
+
+const getRelationship = (relationshipName: string): string =>
+  `${lower(relationshipName)} [shape=diamond, style=filled, fillcolor="${relationshipColor}"]`
 
 const convertER = (code: ER): string => {
   if (Object.entries(code).length === 0) {
@@ -26,6 +33,10 @@ const convertER = (code: ER): string => {
       diagram += getAttribute(ent.id, attribute, isPrimaryKey) + "\n"
       diagram += getConnection(ent.id, attribute) + "\n"
     }
+  }
+
+  for (const rel of code.rel) {
+    diagram += getRelationship(rel.id) + "\n"
   }
 
   diagram += "\n}"
