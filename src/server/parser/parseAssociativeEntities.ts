3075d0e8552a9ed79c25e7c36c074feb2cbaf9b3
Telmo Silva <tvsilva@tech6.com.br>
2020-04-27 23:31:36 -0300

    Removed 'rawData' step

--- src/server/parser/parseAssociativeEntities.ts
+++ src/server/parser/parseAssociativeEntities.ts
@@ -9,48 +9,42 @@ const parseAssociativeEntities = (
     for (const aent of rawAssociativeEntities) {
       const id: string = aent.match(secondWordFound)?.[0] ?? ""
 
-      const rawData = aent.match(allBetweenCurlyBrackets)
-      let data: string[] | null
+      const data: string[] = aent.match(allBetweenCurlyBrackets)?.[0].match(allButWhitespace) ?? []
 
-      if (rawData !== null) {
-        const aent: AEnt = {
-          id,
-          entities: [],
-          attributes: [],
-          pk: [],
-          relationships: []
-        }
+      const associativeEntity: AEnt = {
+        id,
+        entities: [],
+        attributes: [],
+        pk: [],
+        relationships: []
+      }
 
-        data = rawData[0]?.match(allButWhitespace)
-        // const lines = rawData[0].split("\n").filter(line => line != "")
-
-        if (data) {
-          for (let i = 0; i < data.length; i++) {
-            if (data[i+1] && data[i+1][0] == "(") { // entity followed by its cardinality
-              const ent: Conn = {
-                id: data[i],
-                cardinality: data[i+1]
-              }
-              aent.entities.push(ent)
-            } else if (data[i][0] == "*") { // primary key
-              if (data[i-1]) {
-                aent.pk.push(data[i-1])
-              }
-            } else if (data[i][0] != "(") { // attribute
-              aent.attributes.push(data[i])
+      if (data) {
+        for (let i = 0; i < data.length; i++) {
+          if (data[i+1] && data[i+1][0] == "(") { // entity followed by its cardinality
+            const ent: Conn = {
+              id: data[i],
+              cardinality: data[i+1]
             }
+            associativeEntity.entities.push(ent)
+          } else if (data[i][0] == "*") { // primary key
+            if (data[i-1]) {
+              associativeEntity.pk.push(data[i-1])
+            }
+          } else if (data[i][0] != "(") { // attribute
+            associativeEntity.attributes.push(data[i])
           }
         }
-
-        // Get relationships that include this associative entity
-        const rel: Rel[] = relationships.filter(
-          relationship => relationship.entities.filter(
-            entity => entity.id == aent.id).length >= 1)
-        
-        aent.relationships = rel.map(rel => rel.id);
-        
-        associativeEntities.push(aent)
       }
+
+      // Get relationships that include this associative entity
+      const rel: Rel[] = relationships.filter(
+        relationship => relationship.entities.filter(
+          entity => entity.id == associativeEntity.id).length >= 1)
+      
+      associativeEntity.relationships = rel.map(rel => rel.id);
+      
+      associativeEntities.push(associativeEntity)
     }
   }
 
--- src/server/parser/parseSpecializations.ts
+++ src/server/parser/parseSpecializations.ts
@@ -2,37 +2,29 @@ import { Spe } from "../misc/interfaces"
 import { allBetweenCurlyBrackets, allButWhitespace } from "../misc/regex"
 
 const parseSpecializations = (rawSpecializations: string[]): Spe[] => {
+  const specializations: Spe[] = []
+
   if (rawSpecializations) {
-    const specializations: Spe[] = []
-
     for (const spe of rawSpecializations) {
-      const rawData = spe.match(allBetweenCurlyBrackets)
+      const data: string[] = spe.match(allBetweenCurlyBrackets)?.[0].match(allButWhitespace) ?? []
 
-      let data: string[] | null
       let total = false
       let disjoint = false
       const temp = []
 
-      if (rawData !== null) {
-        data = rawData[0].match(allButWhitespace)
-
-        if (data) {
-          const cardinality = data[1].substr(1, 3)
-          if (cardinality[0] === "t") {
-            total = true
-          }
-
-          if (cardinality[2] === "d") {
-            disjoint = true
-          }
-
-          for (let i = 2; i < data.length; i++) {
-            temp.push(data[i])
-          }
+      if (data) {
+        const cardinality = data[1].substr(1, 3)
+        if (cardinality[0] === "t") {
+          total = true
         }
 
-      } else {
-        data = []
+        if (cardinality[2] === "d") {
+          disjoint = true
+        }
+
+        for (let i = 2; i < data.length; i++) {
+          temp.push(data[i])
+        }
       }
 
       if (data) {
