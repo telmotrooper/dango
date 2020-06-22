dbf7a3f5fa88a4d136c87850a166902a1a9fef1f
Telmo "Trooper <telmo.trooper@gmail.com>
2020-06-21 23:09:12 -0300

    Improvements to ErrorBoundary

--- src/client/ErrorBoundary.tsx
+++ src/client/ErrorBoundary.tsx
@@ -1,37 +1,36 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // Source: https://reactjs.org/docs/error-boundaries.html#introducing-error-boundaries
 
 import React, { Component, ErrorInfo } from "react"
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-interface Props { }
+interface Props {
+  diagram: string
+}
 
 interface State {
   hasError: boolean
 }
 
-class ErrorBoundary extends Component {
+class ErrorBoundary extends Component<Props, State> {
   constructor(props: Props) {
     super(props)
     this.state = { hasError: false }
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   static getDerivedStateFromError(error: Error): State {
     // Update state so the next render will show the fallback UI.
     return { hasError: true }
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
     console.log(error)
   }
 
   render(): JSX.Element {
-    if ((this.state as State).hasError) {
+    if (this.state.hasError) {
       return <h1>Something went wrong.</h1>
     }
-    // eslint-disable-next-line react/prop-types
-    return (this.props.children as JSX.Element)
+    return this.props.children as JSX.Element
   }
 }
 
