3e604a8f4bed55850d5d6285adc2924489eb4059
Telmo "Trooper <telmo.trooper@gmail.com>
2020-07-18 19:15:27 -0300

    Wrote more about specializations

--- src/client/modals/help/Specializations.tsx
+++ src/client/modals/help/Specializations.tsx
@@ -12,8 +12,21 @@ export const Specializations = (props: Props): JSX.Element => (
     <p className="paragraph">
       <b>Specializations</b> allow us to declare entities which share attributes with other entities.
     </p>
+
+    <p><b>Completeness:</b></p>
+    <p className="paragraph">
+      A <b>total</b> specialization allows only instances of its children entities to exist, while
+      a <b>partial</b> specialization allows instances of both parent and children entities to exist.
+    </p>
+
+    <p><b>Disjointedness:</b></p>
+    <p className="paragraph">
+      A <b>disjoint</b> specialization defines that an instance of a child entity can be of
+      exactly one specialized type, while an <b>overlap</b> specialization allows them to have one or more specialized types.
+    </p>
+
     <p>Example 1 - Total and disjoint:</p>
     <ExampleCodebox code={specializations} setCode={props.setCode} rows={11} />
-    <p className="paragraph text-after-codebox">TODO: WRITE ABOUT SPECIALIZATIONS HERE.</p>
+    <p className="paragraph text-after-codebox">TODO: WRITE AN EXAMPLE OF A (P,O) SPECIALIZATION.</p>
   </div>
 )
