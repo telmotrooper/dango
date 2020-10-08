d12dcd81efb6cb842a00795a8aab6d066d78ac94
Telmo "Trooper <telmo.trooper@gmail.com>
2020-10-07 23:00:44 -0300

    Improved explanation for union

--- src/client/modals/help/Unions.tsx
+++ src/client/modals/help/Unions.tsx
@@ -12,11 +12,11 @@ interface Props {
 export const Unions = (props: Props): JSX.Element => 
   <Fragment>
     <p>
-      <b>Unions</b> allow us to declare entities as part of a category. Using them, we can define a set of rules that will apply to more than one type of entity.
+      <b>Unions</b> allow us to declare a category or group of entities. Using them, we can define a set of rules that will apply to more than one type of entity.
     </p>
 
     <p>
-      Notice though that, differently from <Link to={helpRoutes.specializations}>{"Specializations"}</Link>, entities in Unions always have both their own type and their Union type. 
+      Notice though that, differently from <Link to={helpRoutes.specializations}>{"Specializations"}</Link>, entities in Unions always have both their own type and their Union type. Also, there is no instance of an entity which is solely of its Union type.
     </p>
 
     <p>Example:</p>
@@ -26,8 +26,10 @@ export const Unions = (props: Props): JSX.Element =>
       </div>
       <div className="column">
         <p>
-          In this example, we define that "Car" and "Truck" belong to category "Vehicle". Since a "Vehicle" has a "plate_number", "Car" and "Truck" will also have this attribute.
-          Also, if we query for all "Vehicles", we'll get all instances of "Cars" and "Trucks".
+          In this example, we define that "Car" and "Truck" belong to category "Vehicle". There's no "Car" or "Truck" that is not also a "Vehicle" and
+          there's no "Vehicle" which is not either a "Car" or a "Truck".
+          Since a "Vehicle" has a "plate_number", "Car" and "Truck" will have this attribute as well.
+          If we query for all "Vehicles", we'll get all instances of "Cars" and "Trucks".
         </p>
       </div>
     </div>
